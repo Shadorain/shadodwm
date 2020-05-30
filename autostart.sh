5474f4caa5578cdd86b1e36640e11d99b5e2a3dc
Shadorain <jordanf7517@gmail.com>
2020-05-30 12:52:26 -0400

    Changed up xinitrc and fixed some autostart options

--- autostart.sh
+++ autostart.sh
@@ -1,6 +1,8 @@
 killall flameshot
 killall picom
+killall sxhkd
 
+sxhkd -c ~/dwm/sxhkdrc &
 picom --experimental-backends &
 feh --bg-scale --no-fehbg ~/Pictures/Backgrounds/cherry.jpg &
 flameshot &
