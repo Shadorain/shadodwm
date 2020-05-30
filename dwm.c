30b49b10039c6bf34b734e748ed7cdab389d4bcf
Shadorain <jordanf7517@gmail.com>
2020-05-30 13:30:21 -0400

    Bar test 8

--- dwm.c
+++ dwm.c
@@ -749,7 +749,7 @@ void
 drawbar(Monitor *m)
 {
 	int x, w, tw = 0;
-	int boxs = drw->fonts->h / 9;
+	int boxs = drw->fonts->h / 7;
 	int boxw = drw->fonts->h / 11 + 0; // 6 + 2 --> 8 + 0
 	unsigned int i, occ = 0, urg = 0;
 	Client *c;
