58db65a3f26a474c32c478be623bcb634837b8d2
Shadorain <jordanf7517@gmail.com>
2020-05-30 13:51:45 -0400

    Bar test 22

--- dwm.c
+++ dwm.c
@@ -770,7 +770,7 @@ drawbar(Monitor *m)
 	for (i = 0; i < LENGTH(tags); i++) {
 		w = TEXTW(tags[i]);
 		drw_setscheme(drw, scheme[m->tagset[m->seltags] & 1 << i ? SchemeSel : SchemeNorm]);
-		drw_text(drw, x, 0, w, bh, lrpad / 1, tags[i], urg & 1 << i);
+		drw_text(drw, x, 0, w, bh, lrpad / 2, tags[i], urg & 1 << i);
 		if (occ & 1 << i)
 			drw_rect(drw, x + boxw, 0, w - ( 1 * boxw + 0), boxw, /*~2 * boxw + 1  -->  1 * boxw + 4*/
 			    m == selmon && selmon->sel && selmon->sel->tags & 1 << i,
