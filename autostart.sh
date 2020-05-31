2f9de4a314d7734f36bf24a16fccb725927700f3
Shadorain <jordanf7517@gmail.com>
2020-05-31 18:02:28 -0400

    FINALLY GOT DWMBLOCKS TO WORK

--- autostart.sh
+++ autostart.sh
@@ -1,8 +1,10 @@
 killall flameshot
 killall picom
 killall sxhkd
+killall dwmblocks
 
 sxhkd -c ~/dwm/sxhkdrc &
 picom --experimental-backends &
 feh --bg-scale --no-fehbg ~/Pictures/Backgrounds/cherry.jpg &
 flameshot &
+~/dwm/dwmblocks/dwmblocks &
