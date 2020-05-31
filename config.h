895770fc165a9321342c460eb693fcc4966306e4
Shadorain <jordanf7517@gmail.com>
2020-05-31 19:03:29 -0400

    Fix font

--- config.h
+++ config.h
@@ -6,7 +6,7 @@ static const unsigned int gappx     = 4;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=12" };
+static const char *fonts[]          = { "'Hack Nerd Font':size=12" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#1b1b29";
 static const char col_gray2[]       = "#1b1b29";
