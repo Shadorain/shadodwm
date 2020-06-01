77725488995439870cf7a1f595d5f64c1dd4ef54
Shadorain <jordanf7517@gmail.com>
2020-06-01 00:01:55 -0400

    SO MUCH GOODNESS ATLASTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT

--- dwmblocks/config.h
+++ dwmblocks/config.h
@@ -1,11 +1,11 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-    //{"",    "mpdup",             0,  0},
-	{"",	"music",	        0,	11},
-	{"",	"clock",	        30,	1},
-	{"",	"volume",	        0,	10},
-	{"",	"battery",	        5,	3},
+    //{"",      "mpdup",            0,  0},
+	{"   ",	"music",	        0,	11},
+	{"",	    "timedate",	        30,	1},
+	{"",	    "volume",	        0,	10},
+	{"",	    "battery",	        5,	3},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char *delim = "   ";
+static char *delim = "  ";
