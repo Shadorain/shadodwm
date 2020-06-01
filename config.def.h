f311aaeeaa45aff65834f577b2ddf036559bbfa4
Shadorain <jordanf7517@gmail.com>
2020-06-01 17:53:09 -0400

    Added comments to almost every line in config.h, have to add headers to all other files as well

--- config.def.h
+++ config.def.h
@@ -17,6 +17,11 @@ static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeStatus]  = { col_gray3, col_gray1,  "#000000"  }, // Statusbar right {text,background,not used but cannot be empty}
+	[SchemeTagsSel]  = { col_gray4, col_cyan,  "#000000"  }, // Tagbar left selected {text,background,not used but cannot be empty}
+    [SchemeTagsNorm]  = { col_gray3, col_gray1,  "#000000"  }, // Tagbar left unselected {text,background,not used but cannot be empty}
+    [SchemeInfoSel]  = { col_gray4, col_cyan,  "#000000"  }, // infobar middle  selected {text,background,not used but cannot be empty}
+    [SchemeInfoNorm]  = { col_gray3, col_gray1,  "#000000"  }, // infobar middle  unselected {text,background,not used but cannot be empty}
 };
 
 /* tagging */
@@ -28,9 +33,9 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     iscentered   isfloating   monitor  scratchkey */
-	{ "Gimp",     NULL,       NULL,       0,            0,           1,           -1,       0  },
     { NULL,       NULL,   "scratchpad",   0,            1,           1,           -1,      's' },
- //	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           0,           -1 },
+	// { "Gimp",    NULL,       NULL,       0,            0,           1,           -1,       0 },
+	//{ "Firefox",  NULL,       NULL,       1 << 8,       0,           0,           -1,       0 },
 };
 
 /* layout(s) */
@@ -43,6 +48,9 @@ static const Layout layouts[] = {
 	{ "  ",      tile },    /* first entry is default */
 	{ "  ",      NULL },    /* no layout function means floating behavior */
 	{ "  ",      monocle },
+	//{ "﩯",      tile },    [> first entry is default <]
+	//{ " ",      NULL },    [> no layout function means floating behavior <]
+	//{ " ",      monocle },
 };
 
 /* key definitions */
